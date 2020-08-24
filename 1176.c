e6e340e4779b3b5b1f577c74169570ce8beaead2
Matheus Costa Monteiro <43221564+costadev00@users.noreply.github.com>
2020-08-24 13:06:36 -0300

    one more

--- 1176.c
+++ 1176.c
@@ -5,20 +5,33 @@
 #include <stdlib.h>
 int main()
 {
-    int t, n, x = 2, aux = 0, vet[999];
+    unsigned long long int vet[61], x, j;
+    int t, n;
     vet[0] = 0;
     vet[1] = 1;
-    scanf("%d", &t);
+    scanf("%lld", &t);
     for (int i = 0; i < t; i++)
     {
-        scanf("%d", &n);
-        while (aux < n)
+        scanf("%lld", &n);
+
+        for (j = 2; j < n + 1; j++)
         {
-            vet[x] = vet[x - 1] + vet[x - 2];
-            x++;
-            aux++;
+            vet[j] = vet[j - 1] + vet[j - 2];
+            x = vet[j];
         }
-        printf("%d\n", vet[x]);
+
+        if (n == 0)
+        {
+            printf("Fib(0) = 0\n");
+        }
+        if (n == 1)
+        {
+            printf("Fib(1) = 1\n");
+        }
+        if (n != 0 && n != 1)
+        {
+            printf("Fib(%lld) = %lld\n", j - 1, x);
+        }
     }
     return 0;
 }
