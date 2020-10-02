b62b3bf1a32275c57e488e726e87aefa36e7f0ea
Matheus Costa Monteiro <43221564+costadev00@users.noreply.github.com>
2020-10-01 22:49:14 -0300

    one more solved

--- 1129.c
+++ 1129.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 int main()
 {
-    int n, a, b, c, d, e;
+    int n, a, b, c, d, e, flag = 0;
     do
     {
         scanf("%d", &n);
@@ -17,7 +17,52 @@ int main()
         for (int i = 0; i < n; i++)
         {
             scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
-            if (a)
+            if (a <= 127)
+            {
+                flag++;
+            }
+            if (b <= 127)
+            {
+                flag++;
+            }
+            if (c <= 127)
+            {
+                flag++;
+            }
+            if (d <= 127)
+            {
+                flag++;
+            }
+            if (e <= 127)
+            {
+                flag++;
+            }
+
+            if (a <= 127 && flag == 1)
+            {
+                printf("A\n");
+            }
+            else if (b <= 127 && flag == 1)
+            {
+                printf("B\n");
+            }
+            else if (c <= 127 && flag == 1)
+            {
+                printf("C\n");
+            }
+            else if (d <= 127 && flag == 1)
+            {
+                printf("D\n");
+            }
+            else if (e <= 127 && flag == 1)
+            {
+                printf("E\n");
+            }
+            else
+            {
+                printf("*\n");
+            }
+            flag = 0;
         }
     } while (n != 0);
     return 0;
