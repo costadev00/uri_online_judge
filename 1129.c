82aa427ab3f223258e75f3298e26d59798f41f52
Matheus Costa Monteiro <43221564+costadev00@users.noreply.github.com>
2020-10-01 22:29:45 -0300

    one more

---
+++ 1129.c
@@ -0,0 +1,24 @@
+/*
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, a, b, c, d, e;
+    do
+    {
+        scanf("%d", &n);
+        if (n == 0)
+        {
+            break;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+            if (a)
+        }
+    } while (n != 0);
+    return 0;
+}
