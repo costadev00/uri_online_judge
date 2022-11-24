4095bb83efc59ce7276d83b2d4a36c67ac074f52
Matheus Costa Monteiro <matheuscostamonteiro.mc@gmail.com>
2022-11-24 15:07:56 -0300

    Update Menor_e_Posição.py

--- Menor_e_Posição.py
+++ Menor_e_Posição.py
@@ -1,12 +1,13 @@
 n = int(input())
+l = []
 l = input().split()
-
-menor = float('inf')
-pos = 1
-for i in range(len(l)):
-    if int(l[i])<menor:
-        menor = int(l[i])
+for i in range(n):
+    l[i] = int(l[i])
+menor = l[0]
+pos = 0
+for i in range(n):
+    if l[i] < menor:
+        menor = l[i]
         pos = i
-
 print("Menor valor: {}".format(menor))
-print("Posicao: {}".format(pos))
+print("Posicao: {}".format(pos)) 
