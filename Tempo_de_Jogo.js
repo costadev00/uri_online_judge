5dfd4ec007bd7450643a9ffccc2068c12af6a669
Matheus Costa Monteiro <matheuscostamonteiro.mc@gmail.com>
2023-07-03 10:50:45 -0300

    Update Tempo_de_Jogo.js

--- Tempo_de_Jogo.js
+++ Tempo_de_Jogo.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const r = fs.readFileSync(0, "utf8");
-const line = r.split(" ");
-const inicio = parseInt(line[0]);
-const fim = parseInt(line[1]);
-const duracao = fim - inicio;
-if (duracao <= 0) console.log("O JOGO DUROU " + (24 + duracao) + " HORA(S)");
-else console.log("O JOGO DUROU " + duracao + " HORA(S)");
+let line = r.split(" ");
+let i = parseInt(line[0]);
+let f = parseInt(line[1]);
+if(i >= f){
+    console.log("O JOGO DUROU " + (24 - i + f) + " HORA(S)");
+}else{
+    console.log("O JOGO DUROU " + (f - i) + " HORA(S)");
+}
